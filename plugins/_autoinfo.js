f94273d4cb2a19fe192a8e3916b1e11c71a83ad6
CRIS OFC <113566411+cagthegame@users.noreply.github.com>
2023-04-22 17:46:55 -0600

    Update _autoinfo.js

--- plugins/_autoinfo.js
+++ plugins/_autoinfo.js
@@ -3,7 +3,7 @@
 </> Recode simple by @NeKosmic
 **/
 
-import db from"../lib/database.js";export async function all(t){const{antiPrivado:a}=db.data.settings[this.user.jid],e=timeString(process.uptime()),i=`<[ ${NombreDelBot} ]>\nTiempo activo : ${e}${a?"|| No chats privados":""}`;let o=0;1*new Date-o>1e3&&(await this.updateProfileStatus(i).catch((t=>t)),o=1*new Date)}
+import db from"../lib/database.js";export async function all(t){const{antiPrivado:a}=db.data.settings[this.user.jid],e=timeString(process.uptime()),i=`CRXS OFC\nTiempo activo : ${e}${a?"|| No chats privados":""}`;let o=0;1*new Date-o>1e3&&(await this.updateProfileStatus(i).catch((t=>t)),o=1*new Date)}
 
 /**
 [_>] https://github.com/NeKosmic/
